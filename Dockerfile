f870bdbc756a50c28237f76fcac58039faedec5b
Kirill Zubkov <kirill131313@gmail.com>
2022-08-20 14:43:34 +0300

    Changes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:17
 
-EXPOSE 8081
+EXPOSE 8080
 
 ADD target/spring_boot_task1-0.0.1-SNAPSHOT.jar myapp.jar
 
--- src/test/java/ru/netology/spring_boot_task1/SpringBootTask1ApplicationTests.java
+++ src/test/java/ru/netology/spring_boot_task1/SpringBootTask1ApplicationTests.java
@@ -23,34 +23,35 @@ class SpringBootTask1ApplicationTests {
     TestRestTemplate testRestTemplate;
 
     @Container
-    private static final GenericContainer<?> prodappContainer = new GenericContainer<>("prodapp")
-            .withExposedPorts(8081);
-    @Container
     private static final GenericContainer<?> devappContainer = new GenericContainer<>("devapp")
             .withExposedPorts(8080);
 
+    @Container
+    private static final GenericContainer<?> prodappContainer = new GenericContainer<>("prodapp")
+            .withExposedPorts(8081);
+
     @BeforeAll
     public static void setUp() {
-        prodappContainer.start();
         devappContainer.start();
+        prodappContainer.start();
     }
 
 
     @Test
     void contextLoadsDevApp() {
-        String HOST = "http://localhost:";
-        Integer port = prodappContainer.getMappedPort(8080);
+        String HOST = "http://192.168.99.100:";
+        Integer port = devappContainer.getMappedPort(8080);
         System.out.println("port: " + port);
         ResponseEntity<String> forEntity = testRestTemplate
                 .getForEntity(HOST + port, String.class);
         System.out.println(forEntity.getBody());
-        Assertions.assertEquals("Current profile is production", forEntity.getBody());
+        Assertions.assertEquals("Current profile is dev", forEntity.getBody());
     }
 
     @Test
     void contextProdApp() {
-        String HOST = "http://localhost:";
-        Integer port = prodappContainer.getMappedPort(8080);
+        String HOST = "http://192.168.99.100:";
+        Integer port = prodappContainer.getMappedPort(8081);
         System.out.println("port: " + port);
         ResponseEntity<String> forEntity = testRestTemplate
                 .getForEntity(HOST + port, String.class);
